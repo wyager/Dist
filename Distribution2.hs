8dcc82f7ddc8b20f2c870037648f5aa7d4c9d377
William Yager <will.yager@gmail.com>
2015-04-14 16:44:11 -0500

    work

--- Distribution2.hs
+++ Distribution2.hs
@@ -30,17 +30,19 @@ import Prelude hiding (product, sum, zipWith)
 import Control.Monad.Random (MonadRandom, Random, getRandomRs)
 import Data.Monoid (Monoid, mempty, mappend)
 import Data.Word (Word)
-import           Data.Set.Strict (Set, member)
-import qualified Data.Set.Strict as Set
+import           Data.Set (Set, member)
+import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
+import Data.List (foldl')
 
 -- | A probability distribution with probabilities of type @p@ and
 -- outcomes/events of type @e@.
-data Distribution p o = Distribution !(DTree p o) !(Set o) !Word
+data Distribution p o = Distribution !(DTree p o) !(Set o) !Word deriving Show
 
 
 data DTree p o = Leaf
-               | DTree !o !p !p !Word !(Distribution p o) !(Distribution p o)
+               | DTree !o !p !p !Word !(DTree p o) !(DTree p o)
+               deriving Show
 
 outcomeOf (DTree o _ _ _ _ _) = o
 probOf    Leaf                = 0
@@ -50,39 +52,50 @@ sumOf     (DTree _ _ s _ _ _) = s
 countOf   Leaf                = 0
 countOf   (DTree _ _ _ c _ _) = c
 leftOf    (DTree _ _ _ _ l _) = l
-leftOf    (DTree _ _ _ _ _ r) = r
+rightOf   (DTree _ _ _ _ _ r) = r
 
 
 
-insert :: (o,p) -> Distribution p o -> Distribution p o
+insert :: (Ord o, Num p, Ord p) => (o,p) -> Distribution p o -> Distribution p o
 insert (o',p') (Distribution tree outcomes dups) = if dups' * 2 < countOf tree
     then Distribution tree' outcomes' dups' -- Not too many repeated elements
-    else fromUniqList . toList $ Distribution tree' empty 0 -- Too many repeats
+    else fromUniqList . toList $ Distribution tree' Set.empty 0
     where
     dups' = if o' `member` outcomes then dups + 1 else dups
     outcomes' = Set.insert o' outcomes
     tree' = insertTree (o',p') tree
 
+reduce :: (Ord o, Num p) => [(o,p)] -> Map.Map o p
 reduce = foldl' (\map (o,p) -> Map.insertWith (+) o p map) Map.empty
 -- | @O(n*log(n))@ amortized. 
-fromList :: [(o,p)] -> Distribution p o
+fromList :: (Ord o, Num p, Ord p) => [(o,p)] -> Distribution p o
 fromList xs = fromUniqList . Map.toList . reduce $ xs
 -- | @O(n*log(n))@.
-toList :: Distribution p o -> [(o,p)]
+toList :: (Ord o, Num p) => Distribution p o -> [(o,p)]
 toList dist = Map.toList . reduce . toRepeatList $ dist
 
 -- | Assumes there are no repeated items in the list. @O(n*log(n))@ amortized.
-fromUniqList :: [(o,p)] -> Distribution p o
+fromUniqList :: (Ord o, Num p, Ord p) => [(o,p)] -> Distribution p o
 fromUniqList xs = Distribution tree Set.empty 0
     where
     tree = foldl' (\tree pair -> insertTree pair tree) Leaf xs
 -- | Doesn't bother to eliminate repeats. @O(n)@
 toRepeatList :: Distribution p o -> [(o,p)]
+toRepeatList = foldrWithP (:) []
+
+-- | A right-associative fold on the tree structure, including the
+-- probabilities. Note that outcomes may be repeated within the data structure.
+foldrWithP :: ((o,p) -> b -> b) -> b -> Distribution p o -> b
+foldrWithP f b (Distribution tree _ _) = foldrTreeWithP f b tree
+
+foldrTreeWithP :: ((o,p) -> b -> b) -> b -> DTree p o -> b
+foldrTreeWithP f b Leaf = b
+foldrTreeWithP f b (DTree o p _ _ l r) = foldrTreeWithP f (f (o,p) (foldrTreeWithP f b r)) l
 
 -- | @O(log(n))@ amortized.
 -- Will not overwrite existing @(o,p)@ pairs in the distribution.
 -- Inserting @(o,p1)@ and @(o,p2)@ is equivalent to inserting @(o,p1+p2)@.
-insertTree :: (o,p) -> DTree p o -> DTree p o
+insertTree :: (Ord o, Num p, Ord p) => (o,p) -> DTree p o -> DTree p o
 insertTree (o',p') Leaf = DTree o' p' p' 1 Leaf Leaf
 insertTree (o',p') (DTree o p s c l r)
     | p' <= p = if countOf l < countOf r
@@ -94,3 +107,4 @@ insertTree (o',p') (DTree o p s c l r)
     where
     s' = s + p
     c' = c + 1
+
