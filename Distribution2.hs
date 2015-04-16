bd196480a074950fbb7b2cc83498a77a26444cb5
William Yager <will.yager@gmail.com>
2015-04-15 22:05:32 -0500

    work

--- Distribution2.hs
+++ Distribution2.hs
@@ -11,12 +11,43 @@
 --
 -- This module provides a data structure and associated functions for
 -- representing discrete probability distributions.
+--
+-- All time and space complexity metrics are given in terms of @n@. In this 
+-- case, @n@ refers to the number of unique outcomes inserted into the tree.
+-- If one were to construct a tree by inserting a billion of the same
+-- outcome, @n@ would still be 1.
 -- 
 -- The data structure is optimized for fast sampling from the distribution.
+-- Sampling ranges from @O(1)@ to @O(log(n))@ depending on the distribution.
+--
+-- Under the hood, the distribution is represented by a perfectly balanced
+-- binary tree. The tree enforces a heap property, where more likely outcomes
+-- are closer to the top than less likely outcomes. Because we're more 
+-- likely to sample from those outcomes, we minimize the amount of time
+-- spent traversing the tree.
+--
+-- When a duplicate outcome is inserted into the tree, the tree's "dups"
+-- counter is incremented. When more than half the tree is duplicate entries,
+-- the entire tree is rebuilt from scratch. Using amortized complexity 
+-- analysis, we can show that insertion is, at worst, @log(n)@ amortized
+-- complexity. This prevents the size of tree from increasing to more than
+-- @O(n)@, even with many duplicate outcomes inserted.
 -----------------------------------------------------------------------------
 
 module Numeric.Probability.Distribution (
-
+    -- * The distribution type
+    Distribution,
+    -- * Probability operations
+    sample,
+    cumulate,
+    normalize,
+    -- * Building
+    empty,
+    insert,
+    fromList,
+    -- * Reducing
+    toList,
+    foldrWithP,
 ) where
 
 import Prelude hiding (product, sum, zipWith)
@@ -29,7 +60,7 @@ import qualified Data.Map.Strict as Map
 import Data.List (foldl')
 
 -- | A probability distribution with probabilities of type @p@ and
--- outcomes/events of type @e@.
+-- outcomes/events of type @o@.
 data Distribution p o = Distribution !(DTree p o) !(Set o) !Word deriving Show
 
 
@@ -48,16 +79,34 @@ leftOf    (DTree _ _ _ _ l _) = l
 rightOf   (DTree _ _ _ _ _ r) = r
 
 
+-- | The sum of all probabilities in the distribution. @O(1)@
+cumulate :: (Num p) => Distribution p o -> p
+cumulate (Distribution tree _ _) = sumOf tree
 
+-- | Normalizes the distribution.
+-- After normalizing, @'cumulate' distribution@ is 1. @O(n)@
+normalize :: (Fractional p) => Distribution p o -> Distribution p o
+normalize (Distribution tree@(DTree _ _ sum _ _ _) members dups) =
+    Distribution (normalize' sum tree) members dups
+normalize' sum Leaf                = Leaf
+normalize' sum (DTree e p s c l r) = DTree e (p/sum) (s/sum) c l' r'
+    where
+    l' = normalize' sum l
+    r' = normalize' sum r
+
+-- | Insert an outcome into the distribution.
+-- Inserting @(o,p1)@ and @(o,p2)@ results in the same sampled distribution as
+-- inserting @(o,p1+p2)@. @O(log(n))@ amortized.
 insert :: (Ord o, Num p, Ord p) => (o,p) -> Distribution p o -> Distribution p o
 insert (o',p') (Distribution tree outcomes dups) = if dups' * 2 <= countOf tree
     then Distribution tree' outcomes' dups' -- Not too many repeated elements
-    else fromUniqList . toList $ Distribution tree' outcomes' 0
+    else fromUniqList . toList $ Distribution tree' outcomes 0
     where
     dups' = if o' `member` outcomes then dups + 1 else dups
     outcomes' = Set.insert o' outcomes
     tree' = insertTree (o',p') tree
 
+-- | The empty distribution. @O(1)@
 empty :: (Num p) => Distribution p o
 empty = Distribution Leaf Set.empty 0
 
@@ -79,6 +128,8 @@ toRepeatList = foldrWithP (:) []
 
 -- | A right-associative fold on the tree structure, including the
 -- probabilities. Note that outcomes may be repeated within the data structure.
+-- If you want identical outcomes to be lumped together, fold on the list 
+-- produced by @'toList'@. @O(n)@.
 foldrWithP :: ((o,p) -> b -> b) -> b -> Distribution p o -> b
 foldrWithP f b (Distribution tree _ _) = foldrTreeWithP f b tree
 
@@ -86,9 +137,6 @@ foldrTreeWithP :: ((o,p) -> b -> b) -> b -> DTree p o -> b
 foldrTreeWithP f b Leaf = b
 foldrTreeWithP f b (DTree o p _ _ l r) = foldrTreeWithP f (f (o,p) (foldrTreeWithP f b r)) l
 
--- | @O(log(n))@ amortized.
--- Will not overwrite existing @(o,p)@ pairs in the distribution.
--- Inserting @(o,p1)@ and @(o,p2)@ is equivalent to inserting @(o,p1+p2)@.
 insertTree :: (Ord o, Num p, Ord p) => (o,p) -> DTree p o -> DTree p o
 insertTree (o',p') Leaf = DTree o' p' p' 1 Leaf Leaf
 insertTree (o',p') (DTree o p s c l r)
@@ -109,8 +157,9 @@ randomPositiveUpto n = do
     return . head . dropWhile (==0) $ randoms
 
 -- | Take a sample from the distribution. Can be used with e.g. @evalRand@
--- or @evalRandIO@ from @Control.Monad.Random@. @O(1)@ for heavily
--- imbalanced distributions, @O(log(n))@ average case, @O(n)$ worst case.
+-- or @evalRandIO@ from @Control.Monad.Random@. @O(log(n))@ for a uniform 
+-- distribution (worst case), but approaches @O(1)@ with less balanced
+-- distributions.
 sample :: (Ord p, Num p, Random p, MonadRandom m) => Distribution p o -> m o
 sample (Distribution tree _ _) = sampleTree tree
 
